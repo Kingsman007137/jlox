89d5aa9340f5a1c048aee3c7633dc485fa110fc3
pan <3398530682@qq.com>
2024-08-29 10:33:59 +0800

    implement lox basic skeleton and error handling

---
+++ src/main/java/com/kingsman/jlox/Lox.java
@@ -0,0 +1,89 @@
+package com.kingsman.jlox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else{
+            runPrompt();
+        }
+    }
+
+    /**
+     * Run the source code from the command line file
+     *
+     * @param path
+     * @throws IOException
+     */
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+    }
+
+    /**
+     * Run the source code from the prompt
+     *
+     * @throws IOException
+     */
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+
+            // If the user makes a mistake, it shouldn’t kill their entire session.
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    /**
+     * Error handling
+     *
+     * @param line
+     * @param message
+     */
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    /**
+     * Report the error
+     *
+     * @param line
+     * @param where
+     * @param message
+     */
+    private static void report(int line, String where, String message) {
+        System.err.println(
+            "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+}
