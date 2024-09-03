b17f74df302845310d58ad953570269829bdf7d1
pan <3398530682@qq.com>
2024-09-03 11:30:32 +0800

    implement generator with to use visitor pattern

---
+++ src/main/java/com/kingsman/tool/GenerateAst.java
@@ -0,0 +1,132 @@
+package com.kingsman.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * prints out the Java code needed to define a class
+ * Ast (Abstract Syntax Tree)
+ * a tiny Java command-line app that generates a file named Expr.java
+ */
+public class GenerateAst {
+    // don't know what args should be
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    /**
+     * generates the Expr.java file
+     *
+     * @param outputDir
+     * @param baseName
+     * @param types
+     * @throws IOException
+     */
+    private static void defineAst(
+            String outputDir, String baseName, List<String> types)
+            throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        // depends on my folder structure
+        writer.println("package com.kingsman.jlox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        // The AST classes.
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
+        writer.println("}");
+        writer.close();
+    }
+
+    /**
+     * generates the Java code for a single expression class
+     * include the class definition, constructor, and fields
+     *
+     * @param writer
+     * @param baseName
+     * @param className
+     * @param fieldList
+     */
+    private static void defineType(
+            PrintWriter writer, String baseName,
+            String className, String fieldList) {
+        writer.println("  static class " + className + " extends " +
+                baseName + " {");
+
+        // Constructor.
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+
+        writer.println("    }");
+
+        // Visitor pattern.
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println("    }");
+
+        // Fields.
+        writer.println();
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        writer.println("  }");
+    }
+
+    /**
+     * generates the Java code for the Visitor interface
+     * (use the visitor pattern to walk the AST)
+     *
+     * @param writer
+     * @param baseName
+     * @param types
+     */
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        // use generics to let each implementation fill in a return type.
+        writer.println("  interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("  }");
+
+    }
+}
