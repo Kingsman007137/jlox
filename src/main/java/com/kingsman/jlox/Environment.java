a36f57b9ba024d0782e5e52627cbb2626b26d3ac
pan <3398530682@qq.com>
2024-09-19 10:52:47 +0800

    implement environment chain

--- src/main/java/com/kingsman/jlox/Environment.java
+++ src/main/java/com/kingsman/jlox/Environment.java
@@ -7,8 +7,19 @@ import java.util.Map;
  * provides a way to store and retrieve variables
  */
 public class Environment {
+    // we chain environments, give each environment a reference to its enclosing one,
+    // walk that chain from innermost out until we find the variable
+    final Environment enclosing;
     // Using the raw string ensures all of those tokens refer to the same map key.
     public final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     /**
      * store the value of a variable in the environment
@@ -25,6 +36,9 @@ public class Environment {
             return values.get(name.lexeme);
         }
 
+        // if the variable is not found in the current environment, check the enclosing one
+        if (enclosing != null) return enclosing.get(name);
+
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
@@ -36,6 +50,12 @@ public class Environment {
         // assignment is not allowed to create a new variable
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
+            return;
+        }
+
+        // if the variable is not found in the current environment, check the enclosing one
+        if (enclosing != null) {
+            enclosing.assign(name, value);
             return;
         }
 
